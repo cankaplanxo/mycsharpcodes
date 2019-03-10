0246b9f78cbf29a47df7c04e0610ebdca02e7051
CAN KAPLAN <32384741+cankaplanxo@users.noreply.github.com>
2019-03-10 20:42:43 +0300

    SICAKLIK DÖNÜŞTÜRME

---
+++ fahrenheit_santigrat_dönüstürme/fahrenheit_santigrat_dönüstürme/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fahrenheit_santigrat_dönüstürme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit, santigrat;
+
+            Console.Write("Fahrenheit sıcaklığını giriniz:");
+            double.TryParse(Console.ReadLine(), out fahrenheit);
+
+            santigrat = (fahrenheit - 32) / 1.8;
+
+            Console.Write("{0} Fahrenheit derece {1:F3} santigrat dereceye eşittir", fahrenheit, santigrat);
+        }
+    }
+}
+
+/*
+           İKİNCİ YOL 
+double fahrenheit, santigrat;
+
+Console.Write("Fahrenheit sıcaklığını giriniz:");
+fahrenheit = double.Parse(Console.ReadLine());
+
+santigrat = (fahrenheit - 32) / 1.8;
+Console.Write("{0} Fahrenheit derece {1:F3} santigrat dereceye eşittir", fahrenheit, santigrat);
+            */
