d017351239920020d72b61faeed0e7fd2d6bf3f3
CAN KAPLAN <32384741+cankaplanxo@users.noreply.github.com>
2019-03-12 21:56:37 +0300

    ÇEMBERİN ÇEVRESİNİ VE ALANINI HESAPLAMA

---
+++ cember_alan_cevre_hesaplama/cember_alan_cevre_hesaplama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cember_alan_cevre_hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double yaricap, cevre, alan;
+                Console.Write("Yarıçapı giriniz:");
+                double.TryParse(Console.ReadLine(), out yaricap);
+                cevre = 2 * 3.141592 * yaricap;
+                alan = 3.141592 * yaricap * yaricap;
+
+                Console.WriteLine("cevre:{0:f2}, alan:{1:f2}", cevre, alan);
+            }
+        }
+    }
+
+
+
+
+
+}
+ 
