7f4280a6bb8e90bd35efb2bbdd56b04ab0ea8403
CAN KAPLAN <32384741+cankaplanxo@users.noreply.github.com>
2019-03-19 00:03:56 +0300

    FİNAL NOTU İLE DERSTEN GEÇİP KALMAYI HESAPLAMA İF ELSE ÖRNEĞİ

---
+++ final_notu_gecti_kaldi/final_notu_gecti_kaldi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_notu_gecti_kaldi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int finalNotu;
+
+            Console.Write("Final Notu Giriniz:");
+            int.TryParse(Console.ReadLine(), out finalNotu);
+
+            if (finalNotu >= 50)
+            {
+                Console.WriteLine("Geçti!");
+            }
+
+            else
+            {
+                Console.WriteLine("Kaldı!");
+            }
+                Console.ReadKey();
+        }
+    }
+}
